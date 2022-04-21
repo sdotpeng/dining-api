9457ab9706aa315103402002d881fc2eb1d1b0fa
Siyuan PENG <siyuan.peng@colby.edu>
2022-04-20 23:32:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def dishes():
 @app.route('/')
 def index():
     # A welcome message to test our server
-    return "<h1>Welcome to our medium-greeting-api!</h1>"
+    return "<h1>Welcome to API for Colby Dining!</h1>"
 
 
 if __name__ == '__main__':
